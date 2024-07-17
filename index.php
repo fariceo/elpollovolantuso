0eaccd577f73a3de14e4785d4e21e580e2943bbc
fariceo <64900696+fariceo@users.noreply.github.com>
2024-07-17 07:07:51 -0500

    2.0.1

--- index.php
+++ index.php
@@ -218,6 +218,7 @@ session_start();
 
 <?php
 //////ponemos un comentario a modo de seguir evaluando los cambios a futuro
+echo "comprobamos algo";
 ?>
 <!--logo-->
 <a href="index.php" onClick="usuario()"><img src="imagenes/logo.jpeg" style="height:50px;width:50px"></a>
